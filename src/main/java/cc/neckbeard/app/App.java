def49465c1be2276ef241f129ad898bca222e3dc
nothub <48992448+nothub@users.noreply.github.com>
2021-07-07 15:57:32 +0200

    fix package

--- src/main/java/cc/neckbeard/app/App.java
+++ src/main/java/cc/neckbeard/app/App.java
@@ -1,4 +1,4 @@
-package cc.neckbeard.app.app;
+package cc.neckbeard.app;
 
 import org.jetbrains.annotations.NotNull;
 import org.tinylog.Logger;
@@ -7,6 +7,7 @@ public class App {
 
     public static void main(@NotNull String[] args) {
         Logger.info("Hello World!");
+        Logger.debug("Goodbye World!");
     }
 
 }
