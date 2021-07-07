2f131827d889f60defb52fa5f4e7987bc84ac52b
nothub <48992448+nothub@users.noreply.github.com>
2021-07-07 02:54:44 +0200

    init

---
+++ src/main/java/cc/neckbeard/app/app/App.java
@@ -0,0 +1,9 @@
+package cc.neckbeard.app.app;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+
+}
---
+++ src/test/java/cc/neckbeard/app/AppTest.java
@@ -0,0 +1,14 @@
+package cc.neckbeard.app;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+public class AppTest {
+
+    @Test
+    public void dummy() {
+        assertTrue(true);
+    }
+
+}
